6260c6d3a2ce46ab958f08b2323963f86803f2fc
ichimgabi97 <ichimgabi97@gmail.com>
2020-04-12 14:52:34 +0300

    Proiect II + Documentatie

---
+++ Ichim_Gabriel_P2_Ma14/Client/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ClientWCF
+{
+    public partial class Form1 : Form
+    {
+        APIClient proxy = new APIClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != null && textBox1 != null && textBox7.Text != null)
+                proxy.AddTables(comboBox1.Text, textBox7.Text, textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // test pentru cautarea dupa nume 
+            pictureBox1.Image = Image.FromFile(proxy.SearchElementByName(comboBox1.Text, textBox1.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            proxy.ModifyElementName(comboBox1.Text, textBox1.Text, textBox8.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           // proxy.DeleteElement(proxy.ReturnIdOfElement(textBox7.Text), comboBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ichim_Gabriel_P2_Ma14/ObjectWCF/API.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyPhotos;
+
+namespace MyphotoWCF
+{
+    public class API : IAPI
+    {
+        MyPhotos.API myPhotos = new MyPhotos.API();
+
+        public void AddTables(String table, String path, String name, String zone, String person, String eventt, String automobile, String otherTags)
+        {
+            myPhotos.AddTables(table, path, name, zone, person, eventt, automobile, otherTags);
+        }
+
+        public String SearchElementByName(String table, String name)
+        {
+            return myPhotos.SearchElementByName(table, name);
+        }
+
+        public String SearchElementByZone(String table, String zone)
+        {
+            return myPhotos.SearchElementByZone(table, zone);
+        }
+
+        public String SearchElementByPerson(String table, String person)
+        {
+            return myPhotos.SearchElementByZone(table, person);
+        }
+
+        public String SearchElementByEvent(String table, String eventt)
+        {
+            return myPhotos.SearchElementByEvent(table, eventt);
+        }
+
+        public String SearchElementByAutomobile(String table, String automobile)
+        {
+            return myPhotos.SearchElementByAutomobile(table, automobile);
+        }
+
+        public String SearchElementByOtherTags(String table, String other_tags)
+        {
+            return myPhotos.SearchElementByOtherTags(table, other_tags);
+        }
+
+        public void ModifyElementName(String table, String name, String newName)
+        {
+            myPhotos.ModifyElementName(table, name, newName);
+        }
+
+        public String ReadTable(String table)
+        {
+            return myPhotos.ReadTable(table);
+        }
+
+        public int ReturnIdOfElement(String path)
+        {
+            return myPhotos.ReturnIdOfElement(path);
+        }
+
+       /* public void DeleteElement(int Id, String table)
+        {
+            myPhotos.DeleteElement(Id, table);
+        }*/
+    }
+}
