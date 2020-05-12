6b319d2cab8682f077202061503184732c7255c3
ichimgabi97 <ichimgabi97@gmail.com>
2020-05-12 15:38:43 +0300

    12/05/2020

---
+++ ASP Net Core WCF EF Razor Pages - Tutorial/ASP Net Core WCF EF Razor Pages - Tutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP_Net_Core_WCF_EF_Razor_Pages___Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
